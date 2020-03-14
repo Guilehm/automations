729dde87bf5bcff12383aafc865351696d5a2f7f
guilherme <guile.hm@hotmail.com>
2020-03-14 17:24:17 -0300

    improve functions

--- crawlers/flights/submarino_flights.py
+++ crawlers/flights/submarino_flights.py
@@ -15,13 +15,30 @@ class SubmarinoFlightsCrawler:
         self.driver = Driver(headless=headless).get_driver()
         self._get_page()
 
+    def wait_for_element(self, condition, value, timeout=5):
+        return WebDriverWait(
+            driver=self.driver,
+            timeout=timeout,
+        ).until(expected_conditions.presence_of_element_located(
+            (condition, value,)
+        ))
+
     def _get_page(self):
         self.driver.get(self.url)
 
-    def find_by_id(self, _id):
-        return self.driver.find_element_by_id(_id)
-
-    def fill_origin_input(self, airport_code='CGH'):
-        self.driver.find_element_by_xpath('//p[@class="cidade"]').click()
-        origin_input = self.find_by_id(_id='pesqinc')
+    def _fill_airport_input(self, airport_code, target):
+        self.driver.find_element_by_xpath(f'//div[@class="{target or "destino"}"]/p[@class="cidade"]').click()
+        origin_input = self.driver.find_element_by_id('pesqinc')
         origin_input.send_keys(airport_code)
+
+    def _click_first_result(self):
+        first_item_xpath = '//div[@class="resultado"]/ul/li/div[@class="item"]'
+        try:
+            self.wait_for_element(By.XPATH, first_item_xpath)
+        except TimeoutException:
+            raise
+        self.driver.find_element_by_xpath(first_item_xpath).click()
+
+    def set_airport(self, airport_code='CGH', target='origem'):
+        self._fill_airport_input(airport_code, target)
+        self._click_first_result()
