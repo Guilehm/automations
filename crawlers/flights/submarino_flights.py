9833bc0c8ba0c8bd88de7b241200c222aeee84d6
guilherme <guile.hm@hotmail.com>
2020-03-14 16:20:18 -0300

    start  submarino crawler creation

---
+++ crawlers/flights/submarino_flights.py
@@ -0,0 +1,22 @@
+from driver.driver_builder import Driver
+
+BASE_URL = 'https://www.submarinoviagens.com.br'
+
+
+class SubmarinoFlightsCrawler:
+
+    def __init__(self, headless=False, url=BASE_URL):
+        self.url = url
+        self.driver = Driver(headless=headless).get_driver()
+        self._get_page()
+
+    def _get_page(self):
+        self.driver.get(self.url)
+
+    def find_by_id(self, _id):
+        return self.driver.find_element_by_id(_id)
+
+    def fill_origin_input(self, airport_code='CGH'):
+        self.driver.find_element_by_xpath('//p[@class="cidade"]').click()
+        origin_input = self.find_by_id(_id='pesqinc')
+        origin_input.send_keys(airport_code)
